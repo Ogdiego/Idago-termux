af3789981a496294cf29105997d54cad8afc3665
Diego <140009460+Idagoo05@users.noreply.github.com>
2023-07-24 22:24:15 -0400

    Actualizar random-C. Ronaldo.js

--- plugins/random-C. Ronaldo.js
+++ plugins/random-C. Ronaldo.js
@@ -1,9 +1,9 @@
-import axios from 'axios'
+/*import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
 let cristiano = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/CristianoRonaldo.json`)).data  
-let ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())]
-conn.sendButton(m.chat, "*Siiiuuuuuu*", author, ronaldo, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], m)}
-handler.help = ['cristianoronaldo', 'cr7']
+let ronaldo = await cristiano[Math.floor(lana.length * Math.random())]
+conn.sendButton(m.chat, /*"**"*/, author, Elizabeth, [[' SIGUIENTE ', `${usedPrefix + command}`]], m)}
+handler.help = ['Lana', 'Lanadelrey']
 handler.tags = ['internet']
-handler.command = /^(cristianoronaldo|cr7)$/i
-export default handler
+handler.command = /^(Lana|lanita|lanadelrey)$/i
+export default handler*/
