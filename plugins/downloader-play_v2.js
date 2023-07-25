f0798a76810f4197f3828ee315e7eaea25f1134d
Diego <140009460+Idagoo05@users.noreply.github.com>
2023-07-25 02:45:35 -0400

    Actualizar downloader-play_v2.js

--- plugins/downloader-play_v2.js
+++ plugins/downloader-play_v2.js
@@ -1,22 +1,61 @@
-import fs from 'fs' 
- import fetch from 'node-fetch' 
+let fetch = require('node-fetch') 
  let handler = async (m, {command, conn, text}) => { 
- if (!text) throw `[❗𝐈𝐍𝐅𝐎❗] 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙵𝙰𝙻𝚃𝙰𝙽𝚃𝙴, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴/𝚃𝙸𝚃𝚄𝙻𝙾 𝙾 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙾 𝚅𝙸𝙳𝙴𝙾 𝙳𝙴 𝚈𝙾𝚄𝚃𝚄𝙱𝙴\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:\n#play.1 Cruel Summer - Taylor Swift*` 
- try { 
+ if (!text) throw `*❰ ❗ ❱ Ingrese el nombre/titulo o link para descargar la canción o vídeo de YouTube*\n\n*Ejemplo*\n*#play.1 Vampire - OlvidoRodrigo*\n*#play.2 Lana del Rey - Cinnamon Girl*` 
  if (command == 'play.1') { 
- conn.reply(m.chat, `*_⏳Sᴇ ᴇsᴛᴀ ᴘʀᴏᴄᴇsᴀɴᴅᴏ Sᴜ ᴀᴜᴅɪᴏ...⏳_*`, m)   
- let res = await fetch("https://violetics.pw/api/media/youtube-play?apikey=beta&query="+text) 
+ let espera = '*❰ ❗ ❱ Aguarde un momento en lo que envío su audio*' 
+ m.reply(espera) 
+ let res = await fetch("https://api.dhamzxploit.my.id/api/ytplaymp3?text="+text) 
  let json = await res.json() 
- conn.sendFile(m.chat, json.result.dlmp3, 'error.mp3', null, m, false, { mimetype: 'audio/mp4' })} 
+ conn.sendFile(m.chat, json.result.url, 'error.mp3', null, m, false, { mimetype: 'audio/mp4' })} 
  if (command == 'play.2') { 
- conn.reply(m.chat, `*_⏳Sᴇ ᴇsᴛᴀ ᴘʀᴏᴄᴇsᴀɴᴅᴏ Sᴜ ᴠɪᴅᴇᴏ...⏳_*`, m) 
- let res = await fetch("https://violetics.pw/api/media/youtube-play?apikey=beta&query="+text) 
+ let espera = '*❰ ❗ ❱ Aguarde un momento en lo que envío su video*' 
+ m.reply(espera) 
+ let res = await fetch("https://api.dhamzxploit.my.id/api/ytplaymp4?text="+text) 
  let json = await res.json() 
- conn.sendFile(m.chat, json.result.dlmp4, 'error.mp4', `_𝐈𝐃𝐀𝐆𝐎 𝐁𝐎𝐓_`, m)} 
- } catch (e) { 
- m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*') 
- }} 
- handler.help = ['play.1' , 'play.2'].map(v => v + ' <texto>') 
- handler.tags = ['downloader'] 
+ conn.sendFile(m.chat, json.result.url, 'error.mp4', `*Diego Bot*`, m)} 
+ } 
  handler.command = ['play.1', 'play.2'] 
- export default handler
+ module.exports = handler
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; 
+ let yts = require('yt-search') 
+ let fetch = require('node-fetch') 
+ const { servers, yta, ytv } = require('../lib/y2mate') 
+ let handler = async (m, { conn, command, text }) => { 
+   if (!text) throw '*📌 Escriba el nombre/título del video o audio a bucar*\n\n*Ejemplo:*\n*#play Billie Eilish - Bellyache*' 
+   let results = await yts(text) 
+   m.reply('🔁 *Descargando...*\n\n*❰ ❗ ❱ Si no obtiene ningun resultado o le sale algun error intente con otro nombre*') 
+   let vid = results.all.find(video => video.seconds < 3600) 
+   if (!vid) throw '*Video/Audio No encontrado* ' 
+   let isVideo = /2$/.test(command) 
+   let { dl_link, thumb, title, filesize, filesizeF} = await (isVideo ? ytv : yta)(vid.url, 'id4') 
+   //let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesizesLimit 
+   conn.sendFile(m.chat, thumb, 'thumbnail.jpg', ` 
+ *🪄 ️Reproductor By Diego Bot 🪄* 
+  
+ 💗 *${title}* 
+ 9:99 ━❍──────── -9:99 
+ ↻     ⊲  Ⅱ  ⊳     ↺ 
+ VOLUME: ▁▂▃▄▅▆▇ 100% 
+  
+ *🎈 Tamaño del archivo:* ${filesizeF} 
+ *🎁 Aguarde un momento en lo que envío su audio/video* 
+ `.trim(), m) 
+   let _thumb = {} 
+   try { if (isVideo) _thumb = { thumbnail: await (await fetch(thumb)).buffer() } } 
+   catch (e) { } 
+   conn.sendFile(m.chat, dl_link, title + '.mp' + (.test(command)), ` 
+ *🎈 Título:* ${title} 
+ *🎁 Tamaño del archivo:* ${filesizeF} 
+ `.trim(), m, false, _thumb || {}) 
+ } 
+ handler.help = ['play', 'play2'].map(v => v + ' <canción >') 
+ handler.tags = ['downloader'] 
+ handler.command = /^play2?$/i 
+ handler.group = false 
+  
+ handler.exp = 0 
+ handler.registrar = false 
+ handler.limit = false 
+  
+ module.exports = handler
