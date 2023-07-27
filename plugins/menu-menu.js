f0c266fc5dc9e156f2b690c49bf059066f0680c8
Ogdiego <140674545+Ogdiego@users.noreply.github.com>
2023-07-27 00:09:03 -0400

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -4,9 +4,7 @@
  try { 
  let pp = imagen4 
  //let vn = './media/menu.mp3' 
- let img =  './8fff1963513f9f63129c120745c18707.jpg
-Ir al archivo
-' 
+ let img =  './8fff1963513f9f63129c120745c18707.jpg' 
  let d = new Date(new Date + 3600000) 
  let locale = 'es' 
  let week = d.toLocaleDateString(locale, { weekday: 'long' }) 
